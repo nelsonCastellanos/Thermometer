0217f7a52a6a5c6adf81f2a2489979bc9cf23d54
Nelson Castellanos <nelson.castellanos@mercadolibre.com.co>
2021-11-03 19:41:29 -0500

    Initial commit

---
+++ app/src/main/java/com/umb/thermometer/MainActivity.java
@@ -0,0 +1,58 @@
+package com.umb.thermometer;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Thermometer thermometer;
+    private float temperature;
+    private Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        thermometer = (Thermometer) findViewById(R.id.thermometer);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        simulateAmbientTemperature();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterAll();
+    }
+
+    private void simulateAmbientTemperature() {
+        timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                temperature = Utils.randInt(-10, 35);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        thermometer.setCurrentTemp(temperature);
+                        getSupportActionBar().setTitle(getString(R.string.app_name) + " : " + temperature);
+                    }
+                });
+            }
+        }, 0, 3500);
+    }
+
+    private void unregisterAll() {
+        timer.cancel();
+    }
+
+}
